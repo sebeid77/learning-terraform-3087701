b5a8e189e77b6b696f6ec2200caa3ab2f48d44e8
sebeid77 <sameh.ebeid@vertexinc.com>
2026-01-29 00:36:55 -0500

    intial commit1

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -------------------------
+# Get latest Bitnami Tomcat AMI
+# -------------------------
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,9 +21,58 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# -------------------------
+# Get default VPC
+# -------------------------
+data "aws_vpc" "default" {
+  default = true
+}
+
+# -------------------------
+# Get a default subnet (any AZ)
+# -------------------------
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# -------------------------
+# Security group in default VPC
+# -------------------------
+resource "aws_security_group" "web" {
+  name   = "web-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# -------------------------
+# EC2 Instance
+# -------------------------
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type = "t3.nano"
+
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
     Name = "HelloWorld"
